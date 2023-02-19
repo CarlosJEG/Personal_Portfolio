be8950ced54e47bef5d95930a8845bcc8f1993d5
CarlosJEG <carlos.jeg98@gmail.com>
2023-02-19 12:24:46 -0300

    Portfolio

--- src/components/divStyle/divStyle.js
+++ src/components/divStyle/divStyle.js
@@ -7,14 +7,7 @@ const DivStyle = () => {
 
   return (
     <>
-        <div className={styles.divStyle}>
-            {div.map((div, i) => (
-              <>
-                <p key={i}>{div}</p>
-                <ChevronRightIcon key={i} className={styles.divIcon}/>
-              </>
-            ))}
-        </div>
+        <div className={styles.divStyle}></div>
     </>
   )
 }
